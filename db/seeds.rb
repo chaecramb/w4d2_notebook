7282885bf518ba7be9ff85f703d3a318d65888ad
Chae Cramb <chaecramb@gmail.com>
2015-10-27 16:50:08 +0000

    generate Note model, create db, seed db

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Note.delete_all
+Note.create!(title: 'CodeClan W4D2 notes', note: 'Learned lots about Rails models and migrations. Made Quiditch app :).')
+Note.create!(title: 'To-do list', note: 'Buy potatoes, go snowboarding, build spaceship, land on mars.')
+Note.create!(title: 'Workout 27/10/15', note: 'Lots of push-ups, rows, pulldowns and shoulder presses')
