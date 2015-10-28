b2e02ce188248fb5f1712bfa2b1b290bc7f32a48
Chae Cramb <chaecramb@gmail.com>
2015-10-28 20:36:06 +0000

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Note.delete_all
-Note.create!(title: 'CodeClan W4D2 notes', note: 'Learned lots about Rails models and migrations. Made Quiditch app :).')
+Note.create!(title: 'CodeClan W4D2 notes', note: 'Learned lots about Rails models and migrations. Made Quiditch app :)')
 Note.create!(title: 'To-do list', note: 'Buy potatoes, go snowboarding, build spaceship, land on mars.')
 Note.create!(title: 'Workout 27/10/15', note: 'Lots of push-ups.')
+Note.create!(title: 'CodeClan W4D3 notes', note: 'Rails associatins and partials today. Lots of fun :)')
+Note.create!(title: 'How to build a table', note: 'Get sticks, get plank, stick sticks to plank.')
 Note.create!(title: 'How to build a treehouse', note: 'Get sticks, climb up tree with sticks, build house in tree.')
 
 Tag.delete_all
@@ -24,5 +26,7 @@ trees = Tag.create!(name: 'Trees')
 Note.first.tags << code_clan << ruby << programming
 Note.first(2).last.tags << to_do
 Note.first(3).last.tags << workout
+Note.first(4).last.tags << code_clan << ruby << programming
+Note.first(5).last.tags << diy
 Note.last.tags << diy << trees
 
